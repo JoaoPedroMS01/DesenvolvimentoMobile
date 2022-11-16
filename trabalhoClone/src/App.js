8aaba1a2ddeed82c8bff130c0adb0e4a8d07a3dc
João Pedro <joaopmacedo63@gmail.com>
2022-11-15 21:34:39 -0300

    trabalho finalizado

--- trabalhoClone/src/App.js
+++ trabalhoClone/src/App.js
@@ -1,10 +1,70 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { styles } from "./styles";
-
 
 export default function App() {
   return (
-  <View style={styles.container}>
-  </View>
+    <>
+      <View style={styles.container1}>
+        <View style={styles.containerSeta}>
+          <Image style={styles.seta} source={require('../assets/seta.png')}></Image>
+        </View>
+        <View style={styles.containerImagem}>
+          <Image style={styles.imagemPlaylist} source={require('../assets/imagem_playlist.jpeg')}></Image>
+        </View>
+      </View>
+      <View style={styles.containerTexto}>
+        <Text style={styles.texto}>Sua mixtape semanal cheia de novas descobertas e pérolas
+          musicais escolhidas só pra você. Atualiza toda segunda.
+        </Text>
+      </View>
+      <View style={styles.containerInfo1}>
+        <Text style={styles.texto2}>Feito para 𝗖𝗮𝗿𝗹𝗼𝘀</Text>
+        <View style={styles.containerDuracao}>
+          <Text style={styles.duracao}>2h16min</Text>
+        </View>
+      </View>
+      <View style={styles.containerInfo2}>
+        <View style={styles.containerIcones}>
+          <Image style={styles.icones} source={require('../assets/coracao.png')}></Image>
+          <Image style={styles.icones} source={require('../assets/download.png')}></Image>
+          <Image style={styles.icones} source={require('../assets/menu.png')}></Image>
+        </View>
+        <View style={styles.containerPlay}>
+          <Image style={styles.play} source={require('../assets/play.png')}></Image>
+        </View>
+      </View>
+      <View style={styles.containerMusicas}>
+        <Image style={styles.capa} source={require('../assets/capa-donda.jpeg')}></Image>
+        <View style={styles.infoMusica}>
+          <Text style={styles.texto3}>Hurricane</Text>
+          <Text style={styles.texto4}>Kanye West</Text>
+        </View>
+        <Image style={styles.menuMusica} source={require('../assets/menu.png')}></Image>
+      </View>
+      <View style={styles.containerMusicas}>
+        <Image style={styles.capa} source={require('../assets/capa-iyrtitl.jpeg')}></Image>
+        <View style={styles.infoMusica}>
+          <Text style={styles.texto3}>Legend</Text>
+          <Text style={styles.texto4}>Drake</Text>
+        </View>
+        <Image style={styles.menuMusica} source={require('../assets/menu.png')}></Image>
+      </View>
+      <View style={styles.containerMusicas}>
+        <Image style={styles.capa} source={require('../assets/capa-iowa.jpeg')}></Image>
+        <View style={styles.infoMusica}>
+          <Text style={styles.texto3}>Disasterpiece</Text>
+          <Text style={styles.texto4}>Slipknot</Text>
+        </View>
+        <Image style={styles.menuMusica} source={require('../assets/menu.png')}></Image>
+      </View>
+      <View style={styles.containerMusicas}>
+        <Image style={styles.capa} source={require('../assets/capa-tlop.jpeg')}></Image>
+        <View style={styles.infoMusica}>
+          <Text style={styles.texto3}>Real Friends</Text>
+          <Text style={styles.texto4}>Kanye West</Text>
+        </View>
+        <Image style={styles.menuMusica} source={require('../assets/menu.png')}></Image>
+      </View>
+    </>
   )
 }
--- trabalhoClone/src/styles.js
+++ trabalhoClone/src/styles.js
@@ -1,7 +1,135 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
+    container1: {
+        width: '100%',
+        height: '37%',
+        flexDirection: 'row',
+        backgroundColor: '#2A3D46'
+    },
+    containerSeta: {
+        width: '20%',
+        height: '18%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '2%',
+        backgroundColor: '#2A3D46'
+    },
+    seta: {
+        width: '48%',
+        height: '48%'
+    },
+    containerImagem: {
+        width: '57%',
+        height: '90%',
+        marginTop: '3%',
+        marginLeft: '3%',
         backgroundColor: '#000'
+    },
+    imagemPlaylist: {
+        width: '100%',
+        height: '100%'
+    },
+    containerTexto: {
+        width: '100%',
+        height: '10%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2A3D46'
+    },
+    texto: {
+        color: '#C2C0C0',
+        fontWeight: 'bold',
+        padding: '2%'
+    },
+    containerInfo1: {
+        width: '100%',
+        height: '10%',
+        backgroundColor: '#1C1D1E',
+        justifyContent: 'center',
+    },
+    texto2: {
+        color: '#C2C0C0',
+        fontWeight: 'bold',
+        marginLeft: '5%'
+    },
+    containerInfo2: {
+        width: '100%',
+        height: '10%',
+        backgroundColor: '#1C1D1E',
+        flexDirection: 'row'
+    },
+    containerDuracao: {
+        width: '55%',
+        height: '37%',
+        backgroundColor: '#1C1D1E',
+        alignItems: 'center',
+    },
+    duracao: {
+        color: '#C2C0C0',
+        fontWeight: 'bold',
+        marginRight: '56%'
+    },
+    containerIcones: {
+        width: '50%',
+        height: '100%',
+        backgroundColor: '#1C1D1E',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+    },
+    icones: {
+        height: '35%',
+        width: '15%',
+        marginBottom: '10%',
+        marginRight: '5%'
+    },
+    containerPlay: {
+        width: '18%',
+        height: '90%',
+        marginLeft: '28%',
+        backgroundColor: '#24E346',
+        borderRadius: 100,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    play: {
+        width: '30%',
+        height: '30%'
+    },
+    containerMusicas: {
+        width: '100%',
+        height: '10%',
+        backgroundColor: '#1C1D1E',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    capa: {
+        width: '15%',
+        height: '75%',
+        marginLeft: '3%'
+    },
+    infoMusica: {
+        width: '45%',
+        height: '75%',
+        marginRight: '22%',
+        backgroundColor: '#1C1D1E'
+    },
+    texto3: {
+        color: '#F6F9F7',
+        fontWeight: 'bold',
+        padding: '2%'
+    },
+    texto4: {
+        color: '#919592',
+        fontWeight: 'bold',
+        fontSize: 13,
+        padding: '2%'
+    },
+    menuMusica: {
+        width: '6%',
+        height: '30%',
+        marginRight: '5%'
     }
 })
