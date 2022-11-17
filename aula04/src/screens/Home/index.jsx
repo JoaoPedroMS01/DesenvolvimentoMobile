d8663066ffcf677e23e30818b6380e8ee7385d39
João Pedro <joaopmacedo63@gmail.com>
2022-11-16 22:10:29 -0300

    Aula 4

---
+++ aula04/src/screens/Home/index.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react"
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, FlatList, Button } from "react-native"
+import { getTasks } from "../../services/tasksClient"
+import Card from "../../components/Card"
+import { styles } from "./styles"
+
+export const Home = ({ navigation }) => {
+    const [task, setTasks] = useState([])
+
+    const fetchData = async () => {
+        const taskList = await getTasks()
+        setTasks(taskList)
+      }
+    
+      useEffect(() => {
+        fetchData()
+      }, [])
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={task}
+                renderItem={Card}
+                keyExtractor={item => item.id}
+            />
+            <Button title="Detalhes" onPress={() => navigation.navigate('Detalhes')} />
+        </View>
+    )
+}
